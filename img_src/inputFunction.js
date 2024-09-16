e691cbfdfe8a0f33ca8b00f1482ba14332dd1da6
milestarricone <milestarricone2@gmail.com>
2024-09-16 08:17:23 -0400

    small commit

--- img_src/inputFunction.js
+++ img_src/inputFunction.js
@@ -1,6 +1,12 @@
 const inputBar = document.getElementById('inputBar');
 const inputButton = document.getElementById('inputButton');
+import householdItems from "./cardObjects";
 
 inputButton.addEventListener('click', () => {
   const input = inputBar.value;
   console.log(input);});
+  if (input === householdItems[0-50].name) {
+    console.log(householdItems[0-50].ability);
+  }
+
+
